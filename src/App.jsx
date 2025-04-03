b2628fcb784f97cb077f6e7b032a95c2b23b4eb0
Lee-04-Git <leemaalgraaff.github@gmail.com>
2025-04-03 19:37:26 +0200

    refactored MovieList component to support dynamic sorting and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.css";
+import Fire from "./assets/fire.png";
+import Star from "./assets/glowing-star.png";
+import Party from "./assets/partying-face.png";
 import Navbar from "./components/Navbar/Navbar";
 import MovieList from "./components/MovieList/MovieList";
 
@@ -7,7 +10,9 @@ const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <MovieList />
+      <MovieList type="popular" title="Popular" emoji={Fire} />
+      <MovieList type="top_rated" title="top-rated" emoji={Star} />
+      <MovieList type="upcoming" title="upcoming" emoji={Party} />
     </div>
   );
 };
